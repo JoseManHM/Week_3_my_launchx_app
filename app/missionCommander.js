b8bc02482fad83d2757af28dde46fe62193b065d
José Manuel Hernández Mayorga <manuelmayorga99@gmail.com>
2022-05-19 11:06:38 -0500

    Exercise one, creation of projects in JS is done with their unit test

---
+++ app/missionCommander.js
@@ -0,0 +1,8 @@
+class MissionCommander{
+    constructor (name){
+        this.name = name;
+    }
+}
+
+//Esta linea nos permite exportar la clase
+module.exports = MissionCommander
---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo");
+console.log(carlo.name);
+const manuel = new MissionCommander("Manuel");
+console.log(manuel.name);
+const jose = new MissionCommander("Jose");
+console.log(jose.name);
